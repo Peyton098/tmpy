8262d7bbea107a4c291df4d5d7f872a8a234b8cf
Peyton098 <96086580+Peyton098@users.noreply.github.com>
2021-12-13 22:43:30 +0100

    Update tmpy.py

--- versions/v.0.0.1/tmpy.py
+++ versions/v.0.0.1/tmpy.py
@@ -1,1 +1,35 @@
+import time, os, glob
+from typing import Tuple
 
+class TMP_Manager :
+
+    def __init__(self) -> None:
+        print("--__INIT__--")
+        self.time = ((int(input("Number of hours : ")))*60)*60
+        self.username = input("Username : ")
+        self.run = True
+        self.path = "C:/Users/" + self.username + "/Desktop/TMP/*"
+        self.start()
+
+    def start(self) -> None :
+        print("--START--")
+        while self.run :
+            if False == self.dir_is_empty() :
+                self.delete_files()
+            self.is_the_time()
+
+    def is_the_time(self) -> None :
+        print("--IS THE TIME--")
+        time.sleep(self.time)
+
+    def delete_files(self) -> None :
+        print("--DELETE FILES--")
+        files = glob.glob(self.path)
+        print(files)
+        for file in files:
+            os.remove(file)
+
+    def dir_is_empty(self) -> None :
+        print("--DIR IS EMPTY--")
+        if len(glob.glob(self.path)) > 0 : return False
+        else : return True
